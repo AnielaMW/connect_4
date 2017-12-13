9155134a66472fdd6e6f8d5d4c9ad83c16c204e7
Aniela M Wolkonowski <anielamw@gmail.com>
2017-12-12 21:23:35 -0500

    include player test to round

--- lib/round.rb
+++ lib/round.rb
@@ -3,6 +3,11 @@ class Round
   attr_accessor
 
   def initialize()
+  end
 
+  def player1
+  end
+
+  def player2
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -10,10 +10,12 @@ describe Round do
   end
 
   it 'should have two players' do
-
+    expect(round.player1).to be_an_instance_of(Player)
+    expect(round.player2).to be_an_instance_of(Player)
   end
 
   it 'should have a winner and a loser' do
-
+    expect(round.winner).to be_an_instance_of(Player)
+    expect(round.loser).to be_an_instance_of(Player)
   end
 end
