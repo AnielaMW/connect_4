7183ef1162785497c75916bccce84d52ecf8e36e
Aniela M Wolkonowski <anielamw@gmail.com>
2017-12-12 20:33:04 -0500

    write initial round tests

---
+++ spec/round_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../lib/player'
+
+describe Round do
+
+  let(:round) { Round.new }
+
+  it 'should be an instance of Round' do
+    expect(round).to be_an_instance_of(Round)
+  end
+
+  it 'should have two players' do
+
+  end
+
+  it 'should have a winner and a loser' do
+
+  end
+end
